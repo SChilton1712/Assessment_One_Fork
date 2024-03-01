1b5d94ebd34efdcd1ba12f6dd74cad69ee772032
SChilton1712 <158061985+SChilton1712@users.noreply.github.com>
2024-03-01 22:11:30 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -2,24 +2,19 @@
 {
     internal class Game
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
-
-        //Methods
         public (int, int, int, int) RollDice() // Public method to instantiate three dice and roll them. Returns the first dice's value, then second, then third, and the sum of the values, all in a tuple.
         {
-            Die FirstDice = new Die(); // Instantiates a new Die object.
-            int FirstValue = FirstDice.Roll(); // Declares an int to store the value returned from the call to the Roll() method of the Die object.
+            // Instantiates a new Die object and declares an int to store the value returned from the call to the Roll() method.
+            Die FirstDice = new Die();
+            int FirstValue = FirstDice.Roll();
 
-            Die SecondDice = new Die(); // Instantiates a new Die object.
-            int SecondValue = SecondDice.Roll(); // Declares an int to store the value returned from the call to the Roll() method of the Die object.
+            // Instantiates a new Die object and declares an int to store the value returned from the call to the Roll() method.
+            Die SecondDice = new Die();
+            int SecondValue = SecondDice.Roll();
 
-            Die ThirdDice = new Die(); // Instantiates a new Die object.
-            int ThirdValue = ThirdDice.Roll(); // Declares an int to store the value returned from the call to the Roll() method of the Die object.
+            // Instantiates a new Die object and declares an int to store the value returned from the call to the Roll() method.
+            Die ThirdDice = new Die();
+            int ThirdValue = ThirdDice.Roll();
 
             int Total = FirstValue + SecondValue + ThirdValue; // Declares a new int to store the sum of the Roll() values added together.
             return (FirstValue, SecondValue, ThirdValue, Total); // Constructs a tuple to return all the int values together.
