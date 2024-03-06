3a66d75372db1fd010d4eede1f981cf8dc0ad2ef
SChilton1712 <158061985+SChilton1712@users.noreply.github.com>
2024-03-06 11:24:37 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -7,7 +7,6 @@ namespace CMP1903_A1_2324 {
     /// </summary>
     static void Main(string[] args) {
       Testing newTest = new Testing(); // Instantiates a new Testing object.
-      newTest.StartTests(); // Calls the StartTests() method to run tests on the other classes and assert error messages if any discrepancies are found.
       do {
         Game newGame = new Game(); // Instantiates a new Game object.
         Console.WriteLine(newGame.RollDice()); // Outputs the values of the dice and their total to the console.
